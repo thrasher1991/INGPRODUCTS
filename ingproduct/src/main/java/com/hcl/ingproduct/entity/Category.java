563889496236b69da1588bfde5276e111a0c9f3b
User1 <User1@LP-5CD9296CYB.HCLT.CORP.HCL.IN>
2019-10-03 16:56:33 +0530

    Category Test

--- ingproduct/src/main/java/com/hcl/ingproduct/entity/Category.java
+++ ingproduct/src/main/java/com/hcl/ingproduct/entity/Category.java
@@ -7,13 +7,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name="category")
-@Getter
-@Setter
+
 public class Category {
 	@Id
 	@Column(name="category_id")
@@ -21,5 +18,17 @@ public class Category {
 	private int categoryId; 
 	@Column(name="category_name")
 	private String categoryName;
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
 
 }
