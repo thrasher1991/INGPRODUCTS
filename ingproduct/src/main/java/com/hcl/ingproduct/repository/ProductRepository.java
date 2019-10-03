6901a19f74285decc6ee6f1a8968f85b30cd1ca0
Jyoshna <moramjyoshna@gmail.com>
2019-10-03 19:31:09 +0530

    upload File

--- ingproduct/src/main/java/com/hcl/ingproduct/repository/ProductRepository.java
+++ ingproduct/src/main/java/com/hcl/ingproduct/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package com.hcl.ingproduct.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hcl.ingproduct.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+	Optional<Product> findByProductNameAndProductDescription(String productName, String productDescrition);
 }
